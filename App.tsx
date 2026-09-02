1411e770cd35bcb127db947b1f2b02c784c26c34
Sebastian <sebruizmun@gmail.com>
2026-09-01 19:23:25 -0600

    Show uncaught JS errors on-screen instead of crashing silently

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppStoreProvider } from './src/context/AppStore';
 import { RootNavigator } from './src/navigation/RootNavigator';
+import { ErrorBoundary } from './src/components/ErrorBoundary';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <AppStoreProvider>
-        <StatusBar style="dark" />
-        <RootNavigator />
-      </AppStoreProvider>
-    </SafeAreaProvider>
+    <ErrorBoundary>
+      <SafeAreaProvider>
+        <AppStoreProvider>
+          <StatusBar style="dark" />
+          <RootNavigator />
+        </AppStoreProvider>
+      </SafeAreaProvider>
+    </ErrorBoundary>
   );
 }
